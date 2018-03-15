576ffda4bc642b2a7a049bdb3f4b0f9e5a971c0a
Jason <JasonG7234@gmail.com>
2018-03-15 17:29:22 -0400

    Bug fixes

--- lookup.py
+++ lookup.py
@@ -5,6 +5,13 @@ from datetime import *
 from dateutil.relativedelta import *
 import calendar
 
+def findSite(url):
+	
+	response = requests.get(url)
+	html = response.content
+
+	return BeautifulSoup(html, "html.parser")
+
 def getNBADraftNetInfo(name):
 	age_query = "nbadraft net " + name
 	for j in search(age_query, num=1, stop=1):
@@ -19,6 +26,7 @@ def getNBADraftNetInfo(name):
 def getAge(info):
 	birthday = str(info).split("</span>",2)[1]
 	birthday = birthday[1:-43]
+	print(birthday)
 	birthdate = datetime.strptime(birthday, '%m/%d/%y').strftime('%Y-%m-%d')
 	difference_in_years = relativedelta(date.today(), datetime.strptime(birthdate, '%Y-%m-%d')).years
 	difference_in_months = relativedelta(date.today(), datetime.strptime(birthdate, '%Y-%m-%d')).months
@@ -31,7 +39,7 @@ def getClass(info):
 	elif (grade[8] == 'S' and grade[9] == 'o'):
 		grade = grade[8:17]
 	else:
-		grade = grade[8:13]
+		grade = grade[8:14]
 	return grade
 	
 def printShootingNumbers(content):
@@ -52,17 +60,24 @@ def printShootingNumbers(content):
 	
 	
 query = "bkref cbb "
-query += input("BKREF Search: ")
-#print(query)
+test = input("BKREF Search: ")
+query += test
 
 for j in search(query, num=1, stop=1):
 	url = j
 
-response = requests.get(url)
-html = response.content
-
-soup = BeautifulSoup(html, "html.parser")
+soup = findSite(url)
 div = soup.find("div", {"class": "nothumb"})
+	
+if not div: #If the original google search failed, try more direct approach
+	url = "https://www.sports-reference.com/cbb/players/"
+	urlName = test.replace(" ", "-")
+	urlName = urlName.replace("'", "")
+	url += urlName + "-1.html"
+	print(url)
+	soup = findSite(url)
+	div = soup.find("div", {"class": "nothumb"})
+
 name = str(div).split("h1",2)[1]
 name = name[17:-2]
 print("")
