2d0861b81a3dcf540ec5bcbf1ff6f040cb20682f
Jason Gomes <JasonG7234@gmail.com>
2018-03-19 18:52:24 -0400

    Utility methods

--- utils.py
+++ utils.py
@@ -1,4 +1,13 @@
-import lookup
+import requests
+from bs4 import BeautifulSoup
+
+
+def findSite(url):
+	
+	response = requests.get(url)
+	html = response.content
+
+	return BeautifulSoup(html, "html.parser")
 
 def getSecondURL(name): 
 	url = "https://www.sports-reference.com/cbb/players/"
