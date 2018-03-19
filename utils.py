4e09c9a598897aa44fd4c8af14bbe2b5032b2950
Jason Gomes <JasonG7234@gmail.com>
2018-03-19 18:36:10 -0400

    Utility methods

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
+import lookup
+
 def getSecondURL(name): 
 	url = "https://www.sports-reference.com/cbb/players/"
-	urlName = test.replace(" ", "-")
+	urlName = name.replace(" ", "-")
 	urlName = urlName.replace("'", "")
 	url += urlName + "-1.html"
 	return findSite(url)
